7afacb4159c47f27d0047b978619abf1f34beae4
hereiam123 <bdemaio1@gmail.com>
2020-09-02 15:48:32 -0400

    Rendering out burger with some ingredients in builder component

--- burger-builder-classes/src/components/Burger/Burger.js
+++ burger-builder-classes/src/components/Burger/Burger.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import React from "react";
+import Ingredient from "./Ingredient/Ingredient";
+import styles from "./Burger.module.css";
+
 const Burger = (props) => {
-    return ();
-}
+  return (
+    <div className={styles.burger}>
+      <Ingredient type="bread-top" />
+      <Ingredient type="cheese" />
+      <Ingredient type="bacon" />
+      <Ingredient type="salad" />
+      <Ingredient type="meat" />
+      <Ingredient type="bread-bottom" />
+    </div>
+  );
+};
 
 export default Burger;
