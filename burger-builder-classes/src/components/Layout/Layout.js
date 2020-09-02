29b25bf5c03b2d6aefcbfa25fb3c88bf3bf542f4
hereiam123 <bdemaio1@gmail.com>
2020-09-02 14:43:19 -0400

    Starting basic setup

---
+++ burger-builder-classes/src/components/Layout/Layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./Layout.module.css";
+
+const Layout = (props) => (
+  <>
+    <div>Toolbar, Sidedrawer, Backdrop</div>
+    <main className={styles.content}>{props.children}</main>
+  </>
+);
+
+export default Layout;
