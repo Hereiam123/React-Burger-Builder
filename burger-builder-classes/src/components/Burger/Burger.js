974ea893321ef684a1d172d3739dc03d2073e1cf
Brian De Maio <bdemaio1@gmail.com>
2020-09-03 22:16:07 -0400

    Rendering out sandwich ingredients from state handed down by props into Burger component

--- burger-builder-classes/src/components/Burger/Burger.js
+++ burger-builder-classes/src/components/Burger/Burger.js
@@ -3,13 +3,17 @@ import Ingredient from "./Ingredient/Ingredient";
 import styles from "./Burger.module.css";
 
 const Burger = (props) => {
+  /*Map out ingredients from props above into ingredient components*/
+  const ingredients = Object.keys(props.ingredients).map((ingKey) => {
+    console.log(ingKey);
+    return [...Array(props.ingredients[ingKey])].map((_, i) => {
+      return <Ingredient key={ingKey + i} type={ingKey} />;
+    });
+  });
   return (
     <div className={styles.burger}>
       <Ingredient type="bread-top" />
-      <Ingredient type="cheese" />
-      <Ingredient type="bacon" />
-      <Ingredient type="salad" />
-      <Ingredient type="meat" />
+      {ingredients}
       <Ingredient type="bread-bottom" />
     </div>
   );
--- burger-builder-classes/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-classes/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -2,10 +2,18 @@ import React, { Component } from "react";
 import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      cheese: 1,
+      meat: 1,
+    },
+  };
   render() {
     return (
       <>
-        <Burger />
+        <Burger ingredients={this.state.ingredients} />
         <div>Build Controls</div>
       </>
     );
