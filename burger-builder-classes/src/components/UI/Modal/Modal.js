14a257777cc23c3945667e87a9b99456d0712035
hereiam123 <bdemaio1@gmail.com>
2020-09-05 20:36:58 -0400

    Adding backgrop to modal

--- burger-builder-classes/src/components/UI/Modal/Modal.js
+++ burger-builder-classes/src/components/UI/Modal/Modal.js
@@ -1,17 +1,21 @@
 import React from "react";
 import styles from "./Modal.module.css";
+import Backdrop from "../Backdrop/Backdrop";
 
 const Modal = ({ show, children }) => {
   return (
-    <div
-      className={styles.modal}
-      style={{
-        transform: show ? "translateY(0)" : "translateY(-100vh)",
-        opacity: show ? "1" : "0",
-      }}
-    >
-      {children}
-    </div>
+    <>
+      <Backdrop show={show} />
+      <div
+        className={styles.modal}
+        style={{
+          transform: show ? "translateY(0)" : "translateY(-100vh)",
+          opacity: show ? "1" : "0",
+        }}
+      >
+        {children}
+      </div>
+    </>
   );
 };
 
