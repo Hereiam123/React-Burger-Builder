a50ba40b669b3194c2a5c80089fdb7a25acb9c7b
hereiam123 <bdemaio1@gmail.com>
2020-09-05 20:44:04 -0400

    Closing modal on backdrop click

--- burger-builder-classes/src/containers/OrderSummary/OrderSummary.js
+++ burger-builder-classes/src/containers/OrderSummary/OrderSummary.js
@@ -17,6 +17,8 @@ const OrderSummary = ({ ingredients }) => {
       <p>Binding Burger Ingredients</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
+      <button>CANCEL</button>
+      <button>CONTINUE</button>
     </>
   );
 };
