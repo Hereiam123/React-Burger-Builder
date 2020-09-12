baf9fb0ccc7c1e1f555f1a055648aa35b8c983c7
hereiam123 <bdemaio1@gmail.com>
2020-09-12 19:21:18 -0400

    Starting work on NavigationItems

---
+++ burger-builder-classes/src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavigationItem from "./NavigationItem/NavigationItem";
+
+const NavigationItems = () => {
+  return (
+    <ul>
+      <li>
+        <a href="#">Navigation Item</a>
+      </li>
+    </ul>
+  );
+};
+
+export default NavigationItems;
