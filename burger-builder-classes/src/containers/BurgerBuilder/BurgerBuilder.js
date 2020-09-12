243ec682f794305eb0fc6ff562e39e3688648351
hereiam123 <bdemaio1@gmail.com>
2020-09-12 18:53:50 -0400

    Connecting OrderSummary component 'Continue' and 'Cancelled' buttons

--- burger-builder-classes/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-classes/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -77,6 +77,10 @@ class BurgerBuilder extends Component {
     this.setState({ purchasing: false });
   };
 
+  purchaseContinueHandler = () => {
+    alert("You continue!");
+  };
+
   render() {
     /*Check if less button should be disabled for ingredient controls*/
     const disabled = {
@@ -92,7 +96,11 @@ class BurgerBuilder extends Component {
           show={this.state.purchasing}
           modalClosed={this.purchaseCancelHandler}
         >
-          <OrderSummary ingredients={this.state.ingredients} />
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purcahseContinued={this.purchaseContinueHandler}
+          />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
--- burger-builder-classes/src/containers/OrderSummary/OrderSummary.js
+++ burger-builder-classes/src/containers/OrderSummary/OrderSummary.js
@@ -1,7 +1,11 @@
 import React from "react";
 import Button from "../../components/UI/Button/Button";
 
-const OrderSummary = ({ ingredients }) => {
+const OrderSummary = ({
+  ingredients,
+  purchaseCancelled,
+  purcahseContinued,
+}) => {
   //Create ingredient summary
   const ingredientSummary = Object.keys(ingredients).map((ingredient) => {
     return (
@@ -18,8 +22,12 @@ const OrderSummary = ({ ingredients }) => {
       <p>Binding Burger Ingredients</p>
       <ul>{ingredientSummary}</ul>
       <p>Continue to Checkout?</p>
-      <Button btnType="danger">CANCEL</Button>
-      <Button btnType="success">CONTINUE</Button>
+      <Button btnType="danger" clicked={purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="success" clicked={purcahseContinued}>
+        CONTINUE
+      </Button>
     </>
   );
 };
