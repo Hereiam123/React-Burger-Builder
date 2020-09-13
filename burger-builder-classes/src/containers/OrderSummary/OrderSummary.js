d3973d51e0fd02beca9dea10764bdc884effa147
hereiam123 <bdemaio1@gmail.com>
2020-09-13 19:58:12 -0400

    Converting OrderSummary to class component to control updating

--- burger-builder-classes/src/containers/OrderSummary/OrderSummary.js
+++ burger-builder-classes/src/containers/OrderSummary/OrderSummary.js
@@ -1,39 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
 import Button from "../../components/UI/Button/Button";
 
-const OrderSummary = ({
-  ingredients,
-  purchaseCancelled,
-  purcahseContinued,
-  price,
-}) => {
-  //Create ingredient summary
-  const ingredientSummary = Object.keys(ingredients).map((ingredient) => {
-    return (
-      <li key={ingredient}>
-        <span style={{ textTransform: "capitalize" }}>
-          {ingredient}: {ingredients[ingredient]}
-        </span>
-      </li>
+class OrderSummary extends Component {
+  render() {
+    //Create ingredient summary
+    const ingredientSummary = Object.keys(this.props.ingredients).map(
+      (ingredient) => {
+        return (
+          <li key={ingredient}>
+            <span style={{ textTransform: "capitalize" }}>
+              {ingredient}: {this.props.ingredients[ingredient]}
+            </span>
+          </li>
+        );
+      }
     );
-  });
-  return (
-    <>
-      <h3>Your Order</h3>
-      <p>Binding Burger Ingredients</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total: ${price.toFixed(2)}</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button btnType="danger" clicked={purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button btnType="success" clicked={purcahseContinued}>
-        CONTINUE
-      </Button>
-    </>
-  );
-};
+
+    return (
+      <>
+        <h3>Your Order</h3>
+        <p>Binding Burger Ingredients</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total: ${this.props.price.toFixed(2)}</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button btnType="danger" clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button btnType="success" clicked={this.props.purcahseContinued}>
+          CONTINUE
+        </Button>
+      </>
+    );
+  }
+}
 
 export default OrderSummary;
