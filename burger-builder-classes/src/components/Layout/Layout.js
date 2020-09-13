ba6af1464956412061a9492a3ec52d70ffc87002
hereiam123 <bdemaio1@gmail.com>
2020-09-13 19:16:22 -0400

    Converting Layout functional component to class based component to handle Sidebar open/close state

--- burger-builder-classes/src/components/Layout/Layout.js
+++ burger-builder-classes/src/components/Layout/Layout.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { Component } from "react";
 import styles from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import Sidebar from "../Navigation/Sidebar/Sidebar";
 
-const Layout = (props) => (
-  <>
-    <Sidebar />
-    <Toolbar />
-    <div> Sidedrawer, Backdrop</div>
-    <main className={styles.content}>{props.children}</main>
-  </>
-);
+class Layout extends Component {
+  render() {
+    return (
+      <>
+        <Sidebar />
+        <Toolbar />
+        <div> Sidedrawer, Backdrop</div>
+        <main className={styles.content}>{this.props.children}</main>
+      </>
+    );
+  }
+}
 
 export default Layout;
--- burger-builder-classes/src/components/Navigation/Sidebar/Sidebar.js
+++ burger-builder-classes/src/components/Navigation/Sidebar/Sidebar.js
@@ -2,17 +2,21 @@ import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import styles from "./Sidebar.module.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 const Sidebar = () => {
   return (
-    <div className={styles.sidebar}>
-      <div className={styles.logo}>
-        <Logo />
+    <>
+      <Backdrop show />
+      <div className={styles.sidebar}>
+        <div className={styles.logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </>
   );
 };
 
