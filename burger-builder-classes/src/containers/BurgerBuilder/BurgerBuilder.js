e7268ce9b4cc8beaa9eab105e8fed5273baa3775
hereiam123 <bdemaio1@gmail.com>
2020-10-10 23:34:58 -0400

    Updating pruchase state handling

--- burger-builder-classes/src/containers/BurgerBuilder/BurgerBuilder.js
+++ burger-builder-classes/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -27,7 +27,7 @@ class BurgerBuilder extends Component {
 
   componentDidMount() {
     axios
-      .get("https://burger-builder-7be11.firebaseio.com/ingredients.json")
+      .get("/ingredients.json")
       .then((response) => {
         this.setState({ ingredients: response.data });
       })
