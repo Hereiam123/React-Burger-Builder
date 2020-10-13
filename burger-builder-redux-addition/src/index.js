66bf4764a27690f3703a8bb9efee29b87ff0679f
hereiam123 <bdemaio1@gmail.com>
2020-10-13 19:04:27 -0400

    Cleaning up service worker

--- burger-builder-redux-addition/src/index.js
+++ burger-builder-redux-addition/src/index.js
@@ -6,7 +6,6 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import thunk from "redux-thunk";
-import * as serviceWorker from "./serviceWorker";
 import burgerBuilderReducer from "./store/reducers/burdgerBuilder";
 import orderReducer from "./store/reducers/order";
 import authReducer from "./store/reducers/auth";
@@ -34,8 +33,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById("root")
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
