03c475335888f74ee6d0a349e4a7fe9b06436262
hereiam123 <bdemaio1@gmail.com>
2020-10-22 00:07:21 -0400

    Converting orders component to use React hooks

--- burger-builder-hooks/src/containers/Orders/Orders.js
+++ burger-builder-hooks/src/containers/Orders/Orders.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import Order from "../../components/Order/Order";
 import Spinner from "../../components/UI/Spinner/Spinner";
@@ -6,30 +6,28 @@ import axios from "../../axiosInstance";
 import withErroHandler from "../withErrorHandler/withErrorHandler";
 import * as orderActions from "../../store/actions/order";
 
-class Orders extends Component {
-  componentDidMount() {
-    this.props.onInitFetchOrders(this.props.authToken, this.props.userId);
-  }
-  render() {
-    return (
-      <div>
-        {!this.props.loading ? (
-          this.props.orders.map((order) => {
-            return (
-              <Order
-                key={order.id}
-                ingredients={order.ingredients}
-                price={+order.price}
-              />
-            );
-          })
-        ) : (
-          <Spinner />
-        )}
-      </div>
-    );
-  }
-}
+const Orders = (props) => {
+  useEffect(() => {
+    props.onInitFetchOrders(props.authToken, props.userId);
+  }, []);
+  return (
+    <div>
+      {!props.loading ? (
+        props.orders.map((order) => {
+          return (
+            <Order
+              key={order.id}
+              ingredients={order.ingredients}
+              price={+order.price}
+            />
+          );
+        })
+      ) : (
+        <Spinner />
+      )}
+    </div>
+  );
+};
 
 const mapStateToProps = (state) => {
   return {
