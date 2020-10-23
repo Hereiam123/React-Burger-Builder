2018f3e2c29f1161759d0db71db6d203a6b319ed
hereiam123 <bdemaio1@gmail.com>
2020-10-22 22:25:13 -0400

    Converted OrderSummary and Modal components

--- burger-builder-hooks/src/containers/OrderSummary/OrderSummary.js
+++ burger-builder-hooks/src/containers/OrderSummary/OrderSummary.js
@@ -1,39 +1,35 @@
-import React, { Component } from "react";
+import React from "react";
 import Button from "../../components/UI/Button/Button";
 
-class OrderSummary extends Component {
-  render() {
-    //Create ingredient summary
-    const ingredientSummary = Object.keys(this.props.ingredients).map(
-      (ingredient) => {
-        return (
-          <li key={ingredient}>
-            <span style={{ textTransform: "capitalize" }}>
-              {ingredient}: {this.props.ingredients[ingredient]}
-            </span>
-          </li>
-        );
-      }
-    );
-
+const OrderSummary = (props) => {
+  //Create ingredient summary
+  const ingredientSummary = Object.keys(props.ingredients).map((ingredient) => {
     return (
-      <>
-        <h3>Your Order</h3>
-        <p>Binding Burger Ingredients</p>
-        <ul>{ingredientSummary}</ul>
-        <p>
-          <strong>Total: ${this.props.price.toFixed(2)}</strong>
-        </p>
-        <p>Continue to Checkout?</p>
-        <Button btnType="danger" clicked={this.props.purchaseCancelled}>
-          CANCEL
-        </Button>
-        <Button btnType="success" clicked={this.props.purcahseContinued}>
-          CONTINUE
-        </Button>
-      </>
+      <li key={ingredient}>
+        <span style={{ textTransform: "capitalize" }}>
+          {ingredient}: {props.ingredients[ingredient]}
+        </span>
+      </li>
     );
-  }
-}
+  });
+
+  return (
+    <>
+      <h3>Your Order</h3>
+      <p>Burger Ingredients</p>
+      <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total: ${props.price.toFixed(2)}</strong>
+      </p>
+      <p>Continue to Checkout?</p>
+      <Button btnType="danger" clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button btnType="success" clicked={props.purcahseContinued}>
+        CONTINUE
+      </Button>
+    </>
+  );
+};
 
 export default OrderSummary;
